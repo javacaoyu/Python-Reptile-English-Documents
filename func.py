b1ab1ca6f470004f1562bf488412e717859bcf04
曹宇 <yu.cao@esmart365.com>
2019-02-21 17:34:08 +0800

    Get chapter path and chapter title

--- func.py
+++ func.py
@@ -3,15 +3,14 @@
 import requests
 
 
-def is_novel_page(url):
+def is_effective_page(url):
     response = requests.get(url)
     if response.status_code == 200:
-        return True
+        return response
     return False
 
 
-def get_novel_name(url):
-    response = requests.get(url)
+def get_novel_name_by_response(response):
     response.encoding = requests.utils.get_encodings_from_content(response.text)[0]
     content = response.text
     content = content[content.find('<a href="list.html" id="zhangxu">'):]
@@ -19,9 +18,29 @@ def get_novel_name(url):
     start = content.find('>')
     return content[start + 1: 52]
 
-    
+def get_novel_name_by_url(url):
+    response = requests.get(url)
+    return get_novel_name_by_response(response)
 
 
-url = 'http://novel.tingroom.com/shuangyu/2013'
+def get_chapter_list(url):
+    response = requests.get(url)
+    response.encoding = requests.utils.get_encodings_from_content(response.text)[0]
+    content = response.text
+    for line in content.split('\n'):
+        if line.__contains__('title=') and line.__contains__('<li>'):
+            href = line[line.find('href=') + 6:]
+            href = href[:href.find('"')]
+            title = line[line.find('title=') + 7:]
+            title = title[:title.find('"')]
+            print(href, title)
+    # chapters = list()
+    # while True:
+    #     if content.find('title=') == -1:
+    #         break
+    #     content = content.find('title=')
+
+
+url = 'http://novel.tingroom.com/shuangyu/2013/list.html'
 # url = 'http://novel.tingroom.com/ertong/2842'
-print(get_novel_name(url))
+print(get_chapter_list(url))
